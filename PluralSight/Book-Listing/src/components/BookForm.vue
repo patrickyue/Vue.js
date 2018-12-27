3a2ccb6d9cbf3c0bb6edd2287e1fcf86955fcd6e
Patrick.Yue <patrick.yue@blucora.com>
2018-12-27 17:19:37 -0600

    Book-Listing

---
+++ PluralSight/Book-Listing/src/components/BookForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form v-on:submit.prevent="bookSubmit(bookTitle,bookAuthor)" class="" action="#" method="post" >
+    <input type="text" name="title" value="" placeholder="Book Title" v-model="bookTitle">
+    <input type="text" name="author" value="" placeholder="Book Author" v-model="bookAuthor">
+    <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name:'BookForm',
+  props:['books'],
+  data(){
+    return {
+      bookTitle:'',
+      bookAuthor:''
+    }
+  },
+  methods: {
+    bookSubmit(bookTitle, bookAuthor) {
+      this.$emit('addBook', bookTitle, bookAuthor);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ PluralSight/Book-Listing/src/components/BookList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1> {{title}} </h1>
+    <ul>
+      <book-item v-for="book in books" :book="book"></book-item>
+    </ul>
+    <hr>
+    <book-form @addBook='appendBook'></book-form>
+  </div>
+</template>
+
+<script>
+import BookItem from "./BookItem";
+import BookForm from "./BookForm";
+export default {
+  name: "BookList",
+  components:{
+    BookItem,
+    BookForm
+  },
+  methods:{
+    appendBook(bookTitle, bookAuthor){
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    }
+  },
+  data(){
+    return {
+      title:"All Books",
+      books:[
+          {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+          {title: 'American Gods', author: 'Neil Gaiman'},
+          {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+h1, h2{
+  font-weight :normal
+}
+
+ul{
+  list-style-type: none;
+  padding: 0;
+}
+
+</style>
