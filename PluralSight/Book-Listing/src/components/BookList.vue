4a73f1d541d62406f8fd4314a6691c550714d7e2
Patrick.Yue <patrick.yue@blucora.com>
2018-12-28 11:16:10 -0600

    fixed :key issue

--- PluralSight/Book-Listing/src/components/BookList.vue
+++ PluralSight/Book-Listing/src/components/BookList.vue
@@ -2,7 +2,7 @@
   <div>
     <h1> {{title}} </h1>
     <ul>
-      <book-item v-for="book in books" :book="book"></book-item>
+      <book-item v-for="book in books" :key="book"></book-item>
     </ul>
     <hr>
     <book-form @addBook='appendBook'></book-form>
