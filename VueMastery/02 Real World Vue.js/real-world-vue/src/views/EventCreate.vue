55b40b09db1b569ed386e472b9667ef86b022a35
Patrick.Yue <patrick.yue@blucora.com>
2019-01-03 12:13:58 -0600

    getters & mapGetters

--- VueMastery/02 Real World Vue.js/real-world-vue/src/views/EventCreate.vue
+++ VueMastery/02 Real World Vue.js/real-world-vue/src/views/EventCreate.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{user.name}}</h1>
+    <h1>Event By {{user.id}}</h1>
+    <p>There are {{catLength}} categories</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapState(["user", "categories"]),
+    ...mapGetters(["getEventById"])
+  }
+};
+</script>
--- VueMastery/02 Real World Vue.js/real-world-vue/src/views/User.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>This is {{username}} page</h1>
-</template>
-
-<script>
-export default {
-  props: ["username"]
-};
-</script>
