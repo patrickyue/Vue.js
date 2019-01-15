ce5596e66caf8f8f7fbdd717fb82b0d64454a7d5
Patrick.Yue <patrick.yue@blucora.com>
2019-01-15 12:27:58 -0600

    NProgress Version 1

--- VueMastery/02 Real World Vue.js/real-world-vue/src/main.js
+++ VueMastery/02 Real World Vue.js/real-world-vue/src/main.js
@@ -5,6 +5,7 @@ import store from "@/store/store";
 
 import upperFirst from "lodash/upperFirst";
 import camelCase from "lodash/camelCase";
+import "nprogress/nprogress.css";
 
 //automactically global components
 const requireComponent = require.context(
--- VueMastery/02 Real World Vue.js/real-world-vue/src/service/EventService.js
+++ VueMastery/02 Real World Vue.js/real-world-vue/src/service/EventService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import NProgress from "nprogress";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:3000",
@@ -7,6 +8,19 @@ const apiClient = axios.create({
     Accept: "application/json",
     "Content-Type": "application/json"
   }
+});
+
+//version 1 to use progress bar
+// when request is about to go out, start the progress bar
+apiClient.interceptors.request.use(config => {
+  NProgress.start();
+  return config;
+});
+
+// // when response returns, finish the progress bar
+apiClient.interceptors.response.use(response => {
+  NProgress.done();
+  return response;
 });
 
 export default {
